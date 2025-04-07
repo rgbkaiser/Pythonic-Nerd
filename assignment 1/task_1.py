b5c0755ece2175a6377a3bf8a239548a69b60aa1
Kaiser <galactichands07@gmail.com>
2025-04-07 14:34:43 +0530

    initial commit

---
+++ assignment 1/task_1.py
@@ -0,0 +1,9 @@
+# Inputting Number
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print()
+# Print + Calculations
+print("Addition: ",num1+num2)
+print("Substraction: ",num1-num2)
+print("Multiplication: ",num1*num2)
+print("Division: ",num1/num2)
